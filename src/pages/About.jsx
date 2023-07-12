b5f0cc2b90718e5b11173829d7973c8c4326eec4
Gil Shaashua <gilshaashua@gmail.com>
2023-07-13 00:09:22 +0300

    completed about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,6 @@
 import { AboutDescription } from '../cmps/AboutDescription'
+import { Banner } from '../cmps/Banner'
+import { Cilents } from '../cmps/Clients'
 import { MeetDirectors } from '../cmps/MeetDirectors'
 
 export function About() {
@@ -6,6 +8,8 @@ export function About() {
         <section className="about-container full main-layout">
             <AboutDescription />
             <MeetDirectors />
+            <Cilents />
+            <Banner />
         </section>
     )
 }
