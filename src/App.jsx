381809076ad91506ab2174a741f8277d270ba5b9
Gil Shaashua <gilshaashua@gmail.com>
2023-07-18 20:42:14 +0300

    added hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,35 @@ import { AppHeader } from './cmps/AppHeader'
 import { About } from './pages/About'
 import { ContactUs } from './pages/ContactUs'
 import { Home } from './pages/Home'
+import { createContext, useState } from 'react'
+
+export const MenuOpenContext = createContext()
 
 function App() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    function onToggleMenu() {
+        setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen)
+    }
+
     return (
-        <section className="main-app main-layout">
-            <AppHeader />
-            <main className="main-layout full">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact-us" element={<ContactUs />} />
-                </Routes>
-            </main>
-            <AppFooter />
-        </section>
+        <MenuOpenContext.Provider value={isMenuOpen}>
+            <section className="main-app main-layout">
+                <div
+                    className={`blur-screen ${isMenuOpen ? 'menu-open' : ''}`}
+                    onClick={onToggleMenu}
+                ></div>
+                <AppHeader onToggleMenu={onToggleMenu} />
+                <main className="main-layout full">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact-us" element={<ContactUs />} />
+                    </Routes>
+                </main>
+                <AppFooter />
+            </section>
+        </MenuOpenContext.Provider>
     )
 }
 
--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { MenuOpenContext } from '../App'
 
-export function AppHeader() {
+export function AppHeader({ onToggleMenu }) {
+    const isMenuOpen = useContext(MenuOpenContext)
+
     return (
         <header className="app-header full main-layout">
             <div className="app-header-inner-container flex align-center space-between">
@@ -8,13 +12,53 @@ export function AppHeader() {
                     <div className="logo">
                         <h1> myteam </h1>
                     </div>
-                    <nav>
-                        <NavLink to={'/'}> home </NavLink>
-                        <NavLink to={'/about'}>about</NavLink>
+                    <nav className={isMenuOpen ? 'menu-open' : ''}>
+                        <NavLink to={'/'} onClick={onToggleMenu}>
+                            {' '}
+                            home{' '}
+                        </NavLink>
+                        <NavLink to={'/about'} onClick={onToggleMenu}>
+                            about
+                        </NavLink>
+                        <button
+                            className="btn-contact-us-mobile"
+                            onClick={onToggleMenu}
+                        >
+                            <NavLink to={'/contact-us'}>contact us</NavLink>
+                        </button>
+                        <svg
+                            className="btn-close-nav-mobile"
+                            onClick={onToggleMenu}
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="17"
+                            viewBox="0 0 18 17"
+                            fill="none"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M8.99989 6.37869L2.98948 0.368286L0.868164 2.48961L6.87857 8.50001L0.868164 14.5104L2.98948 16.6317L8.99989 10.6213L15.0103 16.6317L17.1316 14.5104L11.1212 8.50001L17.1316 2.48961L15.0103 0.368286L8.99989 6.37869Z"
+                                fill="white"
+                            />
+                        </svg>
                     </nav>
                 </div>
-                <button>
+                <button className="btn-contact-us">
                     <NavLink to={'/contact-us'}>contact us</NavLink>
+                </button>
+                <button className="btn-mobile-nav" onClick={onToggleMenu}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="17"
+                        viewBox="0 0 20 17"
+                        fill="none"
+                    >
+                        <rect width="20" height="3" fill="white" />
+                        <rect y="7" width="20" height="3" fill="white" />
+                        <rect y="14" width="20" height="3" fill="white" />
+                    </svg>
                 </button>
             </div>
         </header>
